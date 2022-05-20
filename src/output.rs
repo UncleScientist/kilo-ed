c2732bcefff365a37133ea3cbb7e950aa9f237e2
Uncle Scientist <unclescientist@gmail.com>
2022-05-20 08:47:38 -0400

    Step 25 - drawing ~ characters

--- src/output.rs
+++ src/output.rs
@@ -1,23 +1,32 @@
 use std::io::{stdout, Stdout, Write};
 
-use crossterm::{cursor, terminal, QueueableCommand, Result};
+use crossterm::{cursor, style::Print, terminal, QueueableCommand, Result};
 use errno::errno;
+
+pub fn editor_draw_rows(stdout: &mut Stdout) -> Result<()> {
+    for row in 0..24 {
+        stdout
+            .queue(cursor::MoveTo(0, row))?
+            .queue(Print("~".to_string()))?;
+    }
+
+    Ok(())
+}
 
 pub fn clear_screen(stdout: &mut Stdout) -> Result<()> {
     stdout
         .queue(terminal::Clear(terminal::ClearType::All))?
         .queue(cursor::MoveTo(0, 0))?
-        .flush()?;
-    Ok(())
+        .flush()
 }
 
 pub fn editor_refresh_screen() -> Result<()> {
     let mut stdout = stdout();
 
     clear_screen(&mut stdout)?;
+    editor_draw_rows(&mut stdout)?;
 
-    stdout.flush()?;
-    Ok(())
+    stdout.queue(cursor::MoveTo(0, 0))?.flush()
 }
 
 pub fn die<S: Into<String>>(message: S) {
